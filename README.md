5e464ef7d228f8d05b839d4cd253ba2d5fd08578
Dmytro Mishchenko <dmytro.mishchenko@stanga.net>
2022-01-28 23:31:10 +0200

    feature/menu: Created menu component

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # VpEcosystem
 
-# Darina and Dmytro
+# Information about project
+
+This project idea is to create universal system for schedule, food ordering, news, etc for VPSoftware
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 import Header from "./Header/header";
 import Form from "./Form/form";
 import Menu from "./Menu/menu";
 
+export const MenuContext = React.createContext<boolean>(false);
+
 const App: React.FC = () => {
+  const [menuIsOpened, setMenuIsOpened] = useState<boolean>(true);
+  const openMenuHandler = () => {
+    setMenuIsOpened(!menuIsOpened);
+  };
   return (
-    <>
-      <Header />
-      {/* <Form/> */}
-      <Menu />
-    </>
+    <MenuContext.Provider value={menuIsOpened}>
+      <BrowserRouter>
+        <Header openMenuHandler={openMenuHandler} />
+        <Menu openMenuHandler={openMenuHandler} />
+        <Routes>
+          <Route path="/settings" element={<Form />} />
+          <Route path="/profile" />
+        </Routes>
+      </BrowserRouter>
+    </MenuContext.Provider>
   );
 };
 
